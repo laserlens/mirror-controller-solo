142de298a8ecd2a07d4eab0f66a8896ff5a84018
laserlens <laserlens@gmail.com>
2016-12-14 17:08:28 -0600

    fixed missing codes in build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ cp node_modules/bootstrap/dist/css/bootstrap.min.css.map public/vendors;
 cp node_modules/angular-route/angular-route.min.js public/vendors;
 cp node_modules/angular-route/angular-route.min.js.map public/vendors;
 
-cp node_modules/jquery/dist/jquery.min.js/vendors;
-cp node_modules/jquery/dist/jquery.min.map/vendors;
+cp node_modules/jquery/dist/jquery.min.js public/vendors;
+cp node_modules/jquery/dist/jquery.min.map public/vendors;
 
-cp node_modules/jquery-ui-dist/jquery-ui.min.js/vendors;
+cp node_modules/jquery-ui-dist/jquery-ui.min.js public/vendors;
